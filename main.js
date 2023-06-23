e108a71724d35c0adbe8ffb992f39dab28c49016
Francisco Lima <114367537+limafrancr@users.noreply.github.com>
2023-06-23 15:28:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const name = document.querySelector(".name")
+
+
+function mudarCor(){
+    name.style.color = "blue"
+    name.style.color = "green"
+    name.style.color = "orange"
+    name.style.color = "black"
+}
+
+
+
+function mudarCor(color) {
+    name.style.color = color;
+ }
+ 
+ 
+
+ 
+
+ 
+
+
+
+
+ 
+  
